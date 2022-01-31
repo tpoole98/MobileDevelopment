cf62fc15645cbf84b63db010dac957d4e2fe05d4
tpoole98 <71100521+tpoole98@users.noreply.github.com>
2022-01-31 15:41:27 -0500

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,68 @@
+/*
+HW 02
+HW02
+Tristan Lee Poole
+ */
+
+package com.example.hw02;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+
+import java.util.ArrayList;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayList<Task> list = new ArrayList<>();
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Task t = new Task("HW", "07", "High");
+        list.add(t);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //builder.setMessage(R.string.dialog_message)
+                onCreateDialog();
+
+
+
+        }
+
+    });
+    }
+    public void getActivity(){
+
+    }
+
+        public void onCreateDialog(){
+            //CharSequence[] objects = list.toArray();
+            CharSequence[] objects = {"foo", "coo"};
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Select Task");
+            builder.setItems(objects, new DialogInterface.OnClickListener(){
+                public void onClick(DialogInterface dialog, int which){
+
+                }
+            });
+
+            builder.create();
+            builder.show();
+
+        }
+}
+
---
+++ Task.java
@@ -0,0 +1,13 @@
+package com.example.hw02;
+
+public class Task {
+    String name;
+    String date;
+    String priority;
+    public Task(String n, String d, String p){
+        this.name = n;
+        this.date = d;
+        this.priority = p;
+    }
+
+}
