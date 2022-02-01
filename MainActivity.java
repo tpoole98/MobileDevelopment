0083b913988fee2d87c114fe0a54df91c3597a34
tpoole98 <71100521+tpoole98@users.noreply.github.com>
2022-01-31 21:19:54 -0500

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -14,6 +14,8 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -22,8 +24,9 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     ArrayList<Task> list = new ArrayList<>();
-
-
+    final static public String USE_KEY = "USE KEY";
+    int num;
+    TextView numTask;
 
 
 
@@ -31,13 +34,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Task t = new Task("HW", "07", "High");
+        Task t = new Task("HW", "07", "High", 0);
         list.add(t);
+        Task m = new Task("bn", "07", "High", 0);
+        list.add(m);
+        Task r = new Task("rn", "07", "High", 0);
+        list.add(r);
 
+        //Pull what task to delete
+        if (getIntent() != null && getIntent().getExtras() != null && getIntent().hasExtra(DisplayTask.DATA_ENTERED)) {
+            int task = getIntent().getIntExtra(DisplayTask.DATA_ENTERED, 9);
+            num = task;
+            list.remove(num);
+        }
+        //set Text for num of Task
+        numTask = findViewById(R.id.textView);
+        numTask.setText("You have " + list.size() + " tasks");
+
+
+        //View Tasks --> Calls Alert Box
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //builder.setMessage(R.string.dialog_message)
                 onCreateDialog();
 
 
@@ -45,6 +63,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
     });
+        //Create Task
         findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view){
@@ -55,21 +74,35 @@ public class MainActivity extends AppCompatActivity {
         });
 
         }
-    public void getActivity(){
 
-    }
-
+        //(Alert Box)
         public void onCreateDialog(){
             //CharSequence[] objects = list.toArray();
-            CharSequence[] objects = {"foo", "coo"};
+            CharSequence[] myObjects = new CharSequence[list.size()];
+            for(int i = 0; i< list.size(); i++){
+                myObjects[i] = list.get(i).getName();
+            }
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
             builder.setTitle("Select Task");
-            builder.setItems(objects, new DialogInterface.OnClickListener(){
+            builder.setItems(myObjects, new DialogInterface.OnClickListener(){
                 public void onClick(DialogInterface dialog, int which){
+                    Intent intent = new Intent(MainActivity.this, DisplayTask.class);
+                    intent.putExtra(USE_KEY,
+                            new Task(( list.get(which).getName()),
+                                    (list.get(which).getDate()),
+                                    (( list.get(which).getPriority())),
+                                    which));
 
+                    startActivity(intent);
                 }
             });
-
+             builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int id) {
+                    Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            });
             builder.create();
             builder.show();
 
--- Task.java
+++ Task.java
@@ -1,13 +1,32 @@
 package com.example.hw02;
 
-public class Task {
+import java.io.Serializable;
+
+public class Task implements Serializable {
     String name;
     String date;
     String priority;
-    public Task(String n, String d, String p){
+    int num;
+    public Task(String n, String d, String p, int num){
         this.name = n;
         this.date = d;
         this.priority = p;
+        this.num = num;
+    }
+    public Task(){
+
+    }
+    public String getName(){
+        return name;
+    }
+    public String getDate(){
+        return date;
+    }
+    public String getPriority(){
+        return priority;
+    }
+    public int getNum(){
+        return num;
     }
 
 }
